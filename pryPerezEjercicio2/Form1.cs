6504d88368c8c02468de98112a6026785ced2bd2
Gonzaloperez97 <gonzi.bernardo@gmail.com>
2025-10-27 22:36:52 -0300

    logica de boleto todavia sin completar ,en proceso.

--- pryPerezEjercicio2/Form1.cs
+++ pryPerezEjercicio2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace pryPerezEjercicio2
 {
     public partial class Form1 : Form
@@ -19,6 +21,17 @@ namespace pryPerezEjercicio2
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
+            // 1. Leemos los días. (int) convierte el valor a un número entero.
+            // Supongamos que tu control se llama numericUpDown1
+            int dias = (int)nudCantidad.Value;
+
+            // 2. Leemos la distancia. double.Parse() convierte el texto a un número con decimales.
+            // Supongamos que tu control se llama txtDistancia
+            double distancia = double.Parse(txtDistancia.Text);
+
+            // --- Aquí haremos los cálculos ---
+
+
 
         }
     }
